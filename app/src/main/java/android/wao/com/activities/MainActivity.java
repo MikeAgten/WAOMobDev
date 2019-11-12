d14be451938aa54267031a91b31a4aaa7922bc98
Daan Pinxten <daan.pinxten@student.pxl.be>
2019-11-12 14:23:52 +0100

    Fixed errorhandling for empty fields

--- app/src/main/java/android/wao/com/activities/MainActivity.java
+++ app/src/main/java/android/wao/com/activities/MainActivity.java
@@ -105,13 +105,19 @@ public class MainActivity extends AppCompatActivity {
         String username = user.getText().toString();
         EditText  pass = (EditText) findViewById(R.id.passwordNameEditText);
         String password = pass.getText().toString();
-        signInUser(username,password);
-        if(mAuth.getCurrentUser() != null){
-            Intent intent = new Intent();
-            intent.setClass(this, StoresListActivity.class);
-            Log.d(TAG, "onClick: CLICKED");
-            startActivity(intent);
+        if(username.isEmpty()  || password.isEmpty()){
+            Toast.makeText(MainActivity.this, "Fill in user credentials",
+                    Toast.LENGTH_SHORT).show();
+        }else{
+            signInUser(username,password);
+            if(mAuth.getCurrentUser() != null){
+                Intent intent = new Intent();
+                intent.setClass(this, StoresListActivity.class);
+                Log.d(TAG, "onClick: CLICKED");
+                startActivity(intent);
+            }
         }
+
     }
 
     public void registerButtonClick(View view) {
